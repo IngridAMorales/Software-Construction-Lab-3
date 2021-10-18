611b21ed3544436b0cd10e2f8696f3b8a77629f9
Viviane Fernando <nwarn003@hammer.cs.ucr.edu>
2021-10-18 12:15:10 -0700

    Fixed some errors

--- person.cpp
+++ person.cpp
@@ -16,7 +16,8 @@ Person::Person(const char *name_, Person* father_, Person* mother_){
 }
 
 Person::~Person(){
-    delete children;
+    delete[] children;
+    delete[] name;
 }
 
 void Person::addChild(Person *newChild){
@@ -52,6 +53,7 @@ void Person::printLineage(char dir, int level){
             father->printLineage(dir, level + 1);
         }
     }
+	delete[] temp;
 }
 
 /* helper function to compute the lineage
@@ -66,7 +68,9 @@ char* Person::compute_relation(int level){
     for(int i = 2; i <= level; i++){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
+	delete[] temp;
         temp = temp2;
+	delete[] temp2;
     }
     return temp;
 }
