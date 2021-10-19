b8f8a2f64918eba889a936e770673e4df9164a48
IngridAMorales <ingridmorales1016@gmail.com>
2021-10-18 17:09:33 -0700

    Reverting person.cpp

--- person.cpp
+++ person.cpp
@@ -52,8 +52,6 @@ void Person::printLineage(char dir, int level){
             father->printLineage(dir, level + 1);
         }
     }
-
-    delete[] temp;
 }
 
 /* helper function to compute the lineage
@@ -68,7 +66,6 @@ char* Person::compute_relation(int level){
     for(int i = 2; i <= level; i++){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
-        delete[]  temp;
         temp = temp2;
     }
     return temp;
